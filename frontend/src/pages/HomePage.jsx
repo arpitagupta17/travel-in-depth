9cae66a02d8ddccc4a21638c3e55a71cbbd55191
Arpita Gupta <ag4994425@gmail.com>
2026-06-26 16:52:43 +0530

    Update HomePage with additional components
    Added new components to the HomePage for enhanced features.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import HeroSection from '../components/home/HeroSection'
-import Featured from '../components/layout/Featured';
+// import FeaturedDestinations from "../components/home/FeaturedDestinations";
 import Stats from '../components/stats/Stats';
+import AITripPlanner from "../components/home/AITripPlanner";
+import VisualDiscovery from "../components/home/VisualDiscovery";
+import ExploreByMood from "../components/home/ExploreByMood";
+import TrendingExperiences from "../components/home/TrendingExperiences";
+import HiddenGems from "../components/home/HiddenGems";
+import FestivalCalendar from "../components/home/FestivalCalendar";
+import FinalCTA from "../components/home/FinalCTA";
+import Featured from '../components/layout/Featured';
 function HomePage() {
     return (
         <>
         <HeroSection />
         <Featured/>
+        <AITripPlanner />
+        <VisualDiscovery />
+        <ExploreByMood />
+        <TrendingExperiences />
+        <HiddenGems/>
+        <FestivalCalendar/> 
         <Stats/>
         </>
     )
