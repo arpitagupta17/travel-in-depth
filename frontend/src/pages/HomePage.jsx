a23cf4404b6ad1b6e7507b8e2440d2f7a0ad4669
Arpita Gupta <ag4994425@gmail.com>
2026-06-28 16:39:13 +0530

    Remove unused imports in HomePage component
    Removed commented import for FeaturedDestinations and FinalCTA.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import HeroSection from '../components/home/HeroSection'
-// import FeaturedDestinations from "../components/home/FeaturedDestinations";
 import Stats from '../components/stats/Stats';
 import AITripPlanner from "../components/home/AITripPlanner";
 import VisualDiscovery from "../components/home/VisualDiscovery";
@@ -8,7 +7,6 @@ import ExploreByMood from "../components/home/ExploreByMood";
 import TrendingExperiences from "../components/home/TrendingExperiences";
 import HiddenGems from "../components/home/HiddenGems";
 import FestivalCalendar from "../components/home/FestivalCalendar";
-import FinalCTA from "../components/home/FinalCTA";
 import Featured from '../components/layout/Featured';
 function HomePage() {
     return (
